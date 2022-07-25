8194fe6185fe10d91ce065e49061a98bc72c5725
abdejohan <abdejohan@gmail.com>
2022-07-25 16:50:10 +0200

    Redesign list item for diet screens

--- components/common/ListItem.tsx
+++ components/common/ListItem.tsx
@@ -1,55 +1,44 @@
 import React, { ReactElement } from "react";
-import { View, StyleSheet, TextStyle } from "react-native";
+import { View, TextStyle } from "react-native";
 import { IconButton, List, useTheme } from "react-native-paper";
-import { Text } from "../../typography";
 
 interface ListItemProps {
 	id?: string;
 	listItemStyle?: TextStyle;
 	title: string;
 	titleStyle?: TextStyle;
-	description?: any;
-	descriptionStyle?: TextStyle;
-	icon?: string;
 	onPress?: () => void;
-	left?: any;
-	leftIcon?: ReactElement;
 	rightIcon?: ReactElement;
 	right?: any;
-	percentage?: number | string;
 }
 
 const ListItem: React.FC<ListItemProps> = (props) => {
 	const { colors, roundness } = useTheme();
-	const {
-		// id,
-		icon,
-		listItemStyle,
-		description,
-		descriptionStyle,
-		titleStyle,
-		percentage,
-	} = props;
+	const { listItemStyle, titleStyle } = props;
 
 	return (
 		<View
-			style={[
-				styles.container,
-				{
-					borderRadius: roundness,
-					backgroundColor: colors.surface,
-				},
-			]}>
+			style={{
+				borderRadius: roundness,
+				backgroundColor: colors.surface,
+				width: "100%",
+				marginBottom: 10,
+				height: 64,
+			}}>
 			<List.Item
 				borderless
-				style={[
-					styles.listItem,
-					{ backgroundColor: colors.surface, borderRadius: roundness },
-					listItemStyle,
-				]}
+				style={{
+					backgroundColor: colors.surface,
+					borderRadius: roundness,
+					...listItemStyle,
+				}}
 				titleStyle={[
-					{ fontSize: 16, color: colors.highlightText, fontFamily: "ubuntu-medium" },
-					titleStyle,
+					{
+						fontSize: 16,
+						color: colors.highlightText,
+						fontFamily: "ubuntu-medium",
+						...titleStyle,
+					},
 				]}
 				right={() => (
 					<View
@@ -65,55 +54,10 @@ const ListItem: React.FC<ListItemProps> = (props) => {
 						<IconButton icon='arrow-right' size={18} color={colors.highlightText} />
 					</View>
 				)}
-				descriptionStyle={[{ color: colors.text, fontSize: 16 }, descriptionStyle]}
 				{...props}
-				description={() => (
-					<View style={{ flexDirection: "row", alignItems: "center" }}>
-						{icon && (
-							<IconButton
-								size={13}
-								icon={icon}
-								color={colors.primary}
-								style={{ margin: 0, marginLeft: -5 }}
-							/>
-						)}
-						<Text>{description} </Text>
-						{percentage && <Text style={{ opacity: 0.6 }}>{`(${percentage}%)`}</Text>}
-					</View>
-				)}
 			/>
 		</View>
 	);
 };
 
 export default ListItem;
-
-const styles = StyleSheet.create({
-	container: {
-		width: "100%",
-		marginBottom: 10,
-		height: 64,
-	},
-	date: {
-		fontSize: 16,
-		position: "absolute",
-		marginLeft: 8,
-		top: 13,
-	},
-	listItem: {
-		height: 64,
-		justifyContent: "center",
-		padding: 0,
-		paddingVertical: 10,
-		paddingHorizontal: 10,
-		marginBottom: 10,
-	},
-	leftIcon: {
-		alignItems: "center",
-		justifyContent: "center",
-		width: 44,
-		height: 44,
-		padding: 10,
-		marginRight: 5,
-	},
-});
