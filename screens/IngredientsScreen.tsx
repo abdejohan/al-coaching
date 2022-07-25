ebbaa08bc137bd6f5da2aee62a22b06f09630f01
abdejohan <abdejohan@gmail.com>
2022-07-25 18:12:56 +0200

    Display total nutrient values for meal

--- screens/IngredientsScreen.tsx
+++ screens/IngredientsScreen.tsx
@@ -2,12 +2,19 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { View } from "react-native";
 import { Divider, useTheme } from "react-native-paper";
 import ListItemBasic from "../components/common/ListItemBasic";
-import { Paragraph } from "../typography";
+import { Paragraph, Subheading } from "../typography";
 
 interface DietCategoryProps {
 	navigation: any;
 	route: any;
 }
+
+const calculateTotalNutrientValue = (ingredients: Array<any>, unit: string) => {
+	const sumOfNutrient = ingredients.reduce((accumulator, ingredient) => {
+		return accumulator + ingredient[unit];
+	}, 0);
+	return sumOfNutrient;
+};
 
 const IngredientsScreen: React.FC<DietCategoryProps> = ({ navigation, route }) => {
 	const { meals } = route.params;
@@ -16,23 +23,39 @@ const IngredientsScreen: React.FC<DietCategoryProps> = ({ navigation, route }) =
 	return (
 		<View style={{ paddingHorizontal: 20, backgroundColor: colors.background, flex: 1 }}>
 			{meals ? (
-				<View style={{ borderRadius: roundness, backgroundColor: colors.surface }}>
-					{meals?.map((meal: any) => {
-						return meal?.products?.map((ingredient: any, index: number) => (
-							<View key={index}>
-								<ListItemBasic
-									style={{ padding: 10, marginRight: 10 }}
-									title={ingredient.name}
-									descriptionLeft={`${ingredient.gram}g`} // dot: \u00B7
-									descriptionRight={`${ingredient.kcal} kcal`}
-								/>
-								{meal?.products?.length !== index + 1 && (
-									<Divider style={{ width: "93%", alignSelf: "center" }} />
-								)}
-							</View>
-						));
-					})}
-				</View>
+				<>
+					<View style={{ borderRadius: roundness, backgroundColor: colors.surface }}>
+						{meals?.map((meal: any) => {
+							return meal?.products?.map((ingredient: any, index: number) => (
+								<View key={index}>
+									<ListItemBasic
+										style={{ padding: 10, marginRight: 10 }}
+										title={ingredient.name}
+										descriptionLeft={`${ingredient.gram}g`} // dot: \u00B7
+										descriptionRight={`${ingredient.kcal} kcal`}
+									/>
+									{meal?.products?.length !== index + 1 && (
+										<Divider style={{ width: "93%", alignSelf: "center" }} />
+									)}
+								</View>
+							));
+						})}
+					</View>
+					<View style={{ flexDirection: "row", justifyContent: "center", padding: 20 }}>
+						<Subheading>
+							{`${calculateTotalNutrientValue(
+								meals[0].products,
+								"carbs"
+							)}g Kolhydrater \u00B7 `}
+						</Subheading>
+						<Subheading>
+							{`${calculateTotalNutrientValue(meals[0].products, "fat")}g Fett \u00B7 `}
+						</Subheading>
+						<Subheading>
+							{`${calculateTotalNutrientValue(meals[0].products, "protein")}g Protein`}
+						</Subheading>
+					</View>
+				</>
 			) : (
 				<View>
 					<Paragraph style={{ textAlign: "center", marginTop: 50 }}>
