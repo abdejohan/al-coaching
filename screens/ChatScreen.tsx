e9a0fccbe505dcf377feceea32bcab80de8c9d06
abdejohan <abdejohan@gmail.com>
2022-07-26 16:08:35 +0200

    Change chat user id format

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -16,7 +16,7 @@ const ChatScreen: React.FC = () => {
 	const isFocused = useIsFocused();
 
 	const me: TalkRn.User = {
-		id: user!.id.toString(),
+		id: "AL_" + user!.id.toString(),
 		photoUrl: user?.avatar,
 		name: user?.name,
 		role: "default",
@@ -34,7 +34,7 @@ const ChatScreen: React.FC = () => {
 		}, 2000);
 	}, []);
 
-	const other: TalkRn.User = { id: "1" };
+	const other: TalkRn.User = { id: "AL_1" };
 	const conversationBuilder = TalkRn.getConversationBuilder(TalkRn.oneOnOneId(me, other));
 	conversationBuilder.setParticipant(me);
 	conversationBuilder.setParticipant(other);
